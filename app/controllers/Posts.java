8de90e0617836619ac7763cbecc8ef17e5ce2b88
Willy Aguirre <marti1125@gmail.com>
2013-01-12 23:38:16 -0500

    Adding authentication

--- app/controllers/Posts.java
+++ app/controllers/Posts.java
@@ -7,6 +7,8 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+@Check("admin")
+@With(Secure.class)
 public class Posts extends CRUD{
     
 }
--- app/controllers/Tags.java
+++ app/controllers/Tags.java
@@ -7,6 +7,8 @@ package controllers;
 import play.*;
 import play.mvc.*;
 
+@Check("admin")
+@With(Secure.class)
 public class Tags extends CRUD{
     
 }
